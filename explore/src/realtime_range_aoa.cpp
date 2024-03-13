25c7a149875ecae2e3db90e474a1b86944d131e0
nhjadhav <njadhav@g.harvard.edu>
2024-03-13 13:04:16 -0400

    added profile and variance

--- explore/src/realtime_range_aoa.cpp
+++ explore/src/realtime_range_aoa.cpp
@@ -5,11 +5,14 @@ auto start_aoa_check = std::chrono::high_resolution_clock::now();
 auto end_aoa_check = std::chrono::high_resolution_clock::now();
 auto duration_aoa_check = std::chrono::duration_cast<std::chrono::seconds>(end_aoa_check - start_aoa_check);
 std::string aoa_fn = "/home/react-ws-1/catkin_ws/src/wsr_exploration/data/aoa_val.csv";
-std::ifstream fin;
+std::string aoa_profile_name = "/home/react-ws-1/catkin_ws/src/wsr_exploration/data/aoa_profile.csv";
+std::ifstream fin, aoa_profile;
 std::vector<double> all_range_data, sampled_range_data, top_aoa_peaks;
 bool Flag_get_data_ = false, first_itr=true;
 time_t last_time, current_time_val;
 ros::Publisher range_bearing_publisher;
+double profile_variance = 0;
+std::vector<double> profile_array;
 
 
 void range_bearing_CB(const std_msgs::Float64MultiArray::ConstPtr& msg)
@@ -23,6 +26,7 @@ void range_bearing_CB(const std_msgs::Float64MultiArray::ConstPtr& msg)
         
         sampled_range_data.clear();
         top_aoa_peaks.clear();
+        profile_array.clear();
         
         //Get UWB range value 
         std::random_device rd; // obtain a random number from hardware
@@ -85,9 +89,52 @@ void range_bearing_CB(const std_msgs::Float64MultiArray::ConstPtr& msg)
             current_time_val = strtoul( tokens[0].c_str(), NULL, 0 );
             if( current_time_val > last_time )
             {
-                for(int i = 2; i < tokens.size(); i++) //The first two values are timestamp and TX ID
+                //<timstamp, txid, profile_variance, topN phi angles>
+                profile_variance = stod(tokens[2]);
+                for(int i = 3; i < tokens.size(); i++) //The first two values are timestamp and TX ID
                     top_aoa_peaks.push_back(stod(tokens[i]));
                 
+                //Read AOA profile
+                // aoa_profile.open(aoa_profile_name);
+                // if(aoa_profile.is_open())
+                // {
+                //     std::string line, val;                  /* string for line & value */
+                //     std::vector<std::vector<double>> array;    /* vector of vector<int>  */
+
+                //     while (std::getline (aoa_profile, line)) {        /* read each line */
+                //         std::vector<int> v;                 /* row vector v */
+                //         std::stringstream s (line);         /* stringstream line */
+                //         while (getline (s, val, ','))       /* get each value (',' delimited) */
+                //             v.push_back (std::stod (val));  /* add to row vector */
+                //         array.push_back (v);                /* add row vector to array */
+                //     }
+                //     std::cout << "rows: " << array.size() << " cols: " << array[0].size() << std::endl;
+                // }
+                // aoa_profile.close();
+                
+                aoa_profile.open(aoa_profile_name); 
+                if(aoa_profile.is_open())
+                {
+                    std::string line, val;                  /* string for line & value */
+                       /* vector of vector<int>  */
+
+                    while (std::getline (aoa_profile, line)) 
+                    {        /* read each line */
+                        std::stringstream s (line);         /* stringstream line */
+                        while (getline (s, val, ','))       /* get each value (',' delimited) */
+                            profile_array.push_back (std::stod (val));                /* add row vector to array */
+                    }
+                    std::cout << "elements: " << profile_array.size() << std::endl;
+                }
+                aoa_profile.close();
+
+                
+                // for (auto& row : array) {               /* iterate over rows */
+                //     for (auto& val : row)               /* iterate over vals */
+                //         std::cout << val << "  ";       /* output value      */
+                //     std::cout << "\n";                  /* tidy up with '\n' */
+                // }
+                                
                 last_time = current_time_val;
             }
         }
@@ -107,6 +154,8 @@ void range_bearing_CB(const std_msgs::Float64MultiArray::ConstPtr& msg)
                 ROS_INFO("AOA: %f", top_aoa_peaks[i]);
                 rbmsg.brearing_measurements.push_back(top_aoa_peaks[i]);
             }
+            rbmsg.bearing_profile_variance = profile_variance;
+            rbmsg.aoa_profile = profile_array;
             range_bearing_publisher.publish(rbmsg);
 
         }
