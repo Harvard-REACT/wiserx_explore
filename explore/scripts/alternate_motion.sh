75b806a05aca66b1651b68a076d600f9875ebb6d
nhjadhav <njadhav@g.harvard.edu>
2025-08-13 20:31:22 -0400

    reduced sleep time

--- explore/scripts/alternate_motion.sh
+++ explore/scripts/alternate_motion.sh
@@ -6,12 +6,12 @@ do
 echo "Pub for tb3_1"
 rostopic pub --once /tb3_1/wsr_antenna_motor/start_motion std_msgs/Bool "data: true" & 
 
-sleep 3
+sleep 1
 
 echo "Pub for tb3_2"
 rostopic pub --once /tb3_2/wsr_antenna_motor/start_motion std_msgs/Bool "data: true" &
 
 #Do not comment this out as the rostopic is running in background and will eat up all memory. 
-sleep 3
+sleep 1
 
 done
