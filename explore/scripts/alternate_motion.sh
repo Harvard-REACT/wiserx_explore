667e121af86561aed883d822bd2ad2eb9a5c9602
nhjadhav <njadhav@g.harvard.edu>
2025-10-01 15:21:47 -0400

    end-to-end testing in progress

--- explore/scripts/alternate_motion.sh
+++ explore/scripts/alternate_motion.sh
@@ -4,14 +4,14 @@ itr=0
 while [ $itr -lt 2 ]
 do	
 #echo "Pub for tb3_1"
-rostopic pub --once /tb3_1/wsr_antenna_motor/start_motion std_msgs/Bool "data: true" & 
+rostopic pub --once /tb3_1/wsr_antenna_motor/start_motion std_msgs/Bool "data: true" &
 
-sleep 2.25
+sleep 1.5
 
 echo "Pub for tb3_2"
 rostopic pub --once /tb3_2/wsr_antenna_motor/start_motion std_msgs/Bool "data: true" &
 
 #Do not comment this out as the rostopic is running in background and will eat up all memory. 
-sleep 2.25
+sleep 1.5
 
 done
